8f21156f6f10c18f8895bf48a3dd922a003de7f4
Jonathan Legrand <jonathanamlegrand@gmail.com>
2024-01-31 16:46:08 +0100

    Missing imports

--- bold_dementia/models/transformers.py
+++ bold_dementia/models/transformers.py
@@ -1,4 +1,6 @@
 from sklearn.base import TransformerMixin, BaseEstimator
+from scipy.stats import zscore
+import numpy as np
 from sklearn.pipeline import Pipeline
 
 class Concatenator(TransformerMixin, BaseEstimator):
@@ -10,8 +12,7 @@ class Concatenator(TransformerMixin, BaseEstimator):
 
 class ListScaler(TransformerMixin, BaseEstimator):
     def fit(self, X, y=None):
-        self.scaler = StandardScaler()
         return self
 
     def transform(self, X, y=None):
-        return [self.scaler.fit_transform(X_i) for X_i in X]
+        return [zscore(X_i) for X_i in X]
