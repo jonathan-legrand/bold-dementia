00c471659802121ba30d9d4504b487a6b3fc6bc9
Jonathan Legrand <jonathanamlegrand@gmail.com>
2024-02-14 16:45:06 +0100

    Fast plot of histograms

---
+++ bold_dementia/connectivity/histograms.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def fast_hist(matrix:np.ndarray):
+    """Plot values of arrays containing
+    individuals correlations
+
+    Args:
+        matrix (np.ndarray): (n_subjects, n_regions, r_regions)
+
+    """
+    n_regions = matrix.shape[1]
+    n_subjects = matrix.shape[0]
+    fig, ax = plt.subplots(1, 1)
+
+    # Passing the array is slower
+    for i in range(n_subjects):
+        tst = matrix[i, :, :].reshape((n_regions ** 2))
+        ax.hist(tst, histtype="step")
+    ax.set_xlim(-1, 1)
+    return fig, ax
