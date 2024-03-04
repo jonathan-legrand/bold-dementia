7adb849c394e8ccbb185c6e1a6ad46ed63ae9f0b
Jonathan Legrand <jonathanamlegrand@gmail.com>
2024-03-04 12:50:08 +0100

    Allow custom config

--- bold_dementia/utils/configuration.py
+++ bold_dementia/utils/configuration.py
@@ -4,3 +4,8 @@ def get_config():
     with open('config.yml', 'r') as file:
         config = yaml.safe_load(file)
     return config
+
+def get_custom_config(fpath):
+    with open(fpath, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
