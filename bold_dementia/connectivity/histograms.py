cf2b1bddd957a15563c4323d2d6db29ef91d69e2
Jonathan Legrand <jonathanamlegrand@gmail.com>
2024-03-15 15:04:06 +0100

    Pass existing axes to histogram function

--- bold_dementia/connectivity/histograms.py
+++ bold_dementia/connectivity/histograms.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def fast_hist(matrix:np.ndarray, **plot_kwargs):
+def fast_hist(matrix:np.ndarray, ax=None, **plot_kwargs):
     """Plot values of arrays containing
     individuals correlations
 
@@ -11,7 +11,10 @@ def fast_hist(matrix:np.ndarray, **plot_kwargs):
     """
     n_regions = matrix.shape[1]
     n_subjects = matrix.shape[0]
-    fig, ax = plt.subplots(1, 1)
+    if ax is None:
+        fig, ax = plt.subplots(1, 1)
+    else:
+        fig = None
 
     # Passing the array is slower
     for i in range(n_subjects):
