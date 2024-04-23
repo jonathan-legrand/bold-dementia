987ca1722f4339a70b2b04ec42d8b2752e7255f8
Jonathan Legrand <jonathanamlegrand@gmail.com>
2024-04-23 13:54:34 +0200

    Remove imports

--- bold_dementia/generate_null.py
+++ bold_dementia/generate_null.py
@@ -1,25 +1,18 @@
-import json
 import sys
 from pathlib import Path
 import warnings
 from bold_dementia import get_config
 
 import pandas as pd
-import joblib
 from joblib import Parallel, delayed
 import numpy as np
-from statsmodels.stats.multitest import fdrcorrection
-from statsmodels.tools.sm_exceptions import ConvergenceWarning
 
-from bold_dementia.connectivity import (
-    Atlas, reshape_pvalues, vec_idx_to_mat_idx, z_transform_to_vec, group_groupby, edge_format
-)
 from bold_dementia.models.matrix_GLM import fit_edges
 from bold_dementia.data.volumes import add_volumes
 from utils.saving import save_run
 from utils.iterables import join, all_connectivities
 from bold_dementia.stats.permutations import generate_null
-from bold_dementia.stats.univariate import make_fc_data
+from bold_dementia.stats.univariate import make_fc_data, merge_configs
 
 config = get_config()
 
