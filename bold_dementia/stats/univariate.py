108aa449217e38e16b647df1a41070f477c8b26e
Jonathan Legrand <jonathanamlegrand@gmail.com>
2024-04-23 11:03:35 +0200

    Refactor univariate analysis

--- bold_dementia/stats/univariate.py
+++ bold_dementia/stats/univariate.py
@@ -21,6 +21,72 @@ from bold_dementia.utils.saving import save_run
 from bold_dementia.utils.iterables import join, all_connectivities
 
 config = get_config("/bigdata/jlegrand/AD-prediction/config.yml") #Awful
+# TODO Should probably be refactored in OO way since 
+# we keep passing config around
+
+# TODO Format report only, do not print, and move to utils
+def display_df_report(df):
+    print(f"Testing on {len(df)} subjects")
+    print(f"{df.MA.sum()} AD")
+    print(f"{df.DEMENCE_DAT.isna().sum()} controls")
+
+# TODO Print info about the test, N, etc
+def run_test(df, edges, model_spec):
+    test_df = df.dropna(subset=["NIVETUD"])
+    display_df_report(test_df)
+    fit_df = lambda edge: fit_edges(
+        edge, test_df, model_spec["RHS_FORMULA"], model_spec["GROUPS"]
+    )
+    # TODO check n_jobs in config and set a default value
+    parallel = Parallel(n_jobs=8, verbose=2)
+
+    # TODO This is awfully slow for mixed models and has convergence issues
+    # Change optimizer in statsmodel perhaps?
+    with warnings.catch_warnings(category=ConvergenceWarning, action="ignore"):
+        test_results = parallel(delayed(fit_df)(edge) for edge in edges)
+    return test_results
+
+def export_results(results, edges, maps_specs, model_specs):
+    stats, pvalues = zip(*results)
+
+    print(f"Correcting FDR with {len(pvalues)} comparisons")
+    _, pvalues_corr = fdrcorrection(pvalues)
+
+    maps_name = maps_specs.pop("NAME")
+    model_name = model_specs.pop("NAME")
+    parameters = {**model_specs, **maps_specs}
+    parameters["NAME"] = maps_name + "_" + model_name
+    
+    # The original matrix ordering is preserved 
+    # when no groupby happened,
+    # otherwise it's better to just provide resdf for further
+    # analysis
+    if "BY_BLOCK" not in model_specs.keys() or model_specs["BY_BLOCK"] is False:
+        statmat = reshape_pvalues(stats)
+        pmat = reshape_pvalues(pvalues_corr)
+        pmat_raw = reshape_pvalues(pvalues)
+        matrix_export = {
+            "statmap.joblib": statmat,
+            "pmat.joblib": pmat,
+            "pmat_raw.joblib": pmat_raw
+        }
+        save_run(parameters, joblib.dump, matrix_export, dirkey="statresults")
+
+    resdf = pd.DataFrame({
+        "edges": edges,
+        "beta_AD": stats,
+        "pvalues_raw": pvalues,
+        "pvalues_fdr": pvalues_corr
+    })
+    exppath = save_run(
+        parameters,
+        lambda df, fname: df.to_csv(fname),
+        {"resdf.csv": resdf},
+        dirkey="statresults"
+    )
+
+    print(exppath)
+
 
 def make_fc_data(maps_path, maps_spec, model_spec):
     
@@ -45,7 +111,7 @@ def make_fc_data(maps_path, maps_spec, model_spec):
         edges = AD_fc.columns.to_list()
         
         fc = pd.concat((AD_fc, control_fc)).reset_index(drop=True)
-        #fc = fc.apply(np.arctanh) TODO UNCOMMENT ME PLEASE
+        fc = fc.apply(np.arctanh)
         
         # k is named that way because of numpy
         k:int = 0
