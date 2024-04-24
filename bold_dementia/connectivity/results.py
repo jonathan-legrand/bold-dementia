86e5ee328994a92cdd66446af679aef64b280c73
Jonathan Legrand <jonathanamlegrand@gmail.com>
2024-04-24 17:41:48 +0200

    Pass kwargs to pivot

--- bold_dementia/connectivity/results.py
+++ bold_dementia/connectivity/results.py
@@ -13,7 +13,7 @@ def pivot_resdf(resdf, alpha=0.05):
     resdf_pv = resdf.pivot(index="node_a" ,columns=["node_b"], values=["beta_AD", "pvalues_raw", "pvalues_fdr", "thresholded_beta"])
     return resdf_pv
     
-def edges_to_matrix(df:pd.DataFrame):
+def edges_to_matrix(df:pd.DataFrame, **pivot_kwargs):
     """
     Transform edge values into a full, symmetric, node to node comparison matrix
     """
@@ -21,5 +21,5 @@ def edges_to_matrix(df:pd.DataFrame):
     df["node_b"] = df["edges"].map(lambda edge: edge.split("_")[1])
     swapped = df.rename(columns={"node_a": "node_b", "node_b": "node_a"})
     symmetric_closure = pd.concat((df, swapped))
-    pv = symmetric_closure.pivot(index="node_a" ,columns=["node_b"])
+    pv = symmetric_closure.pivot(index="node_a", columns=["node_b"], **pivot_kwargs)
     return pv
